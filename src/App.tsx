9b56f15e88ae7e23b5dd9da26b84cde801073966
Azhar Dhiyaulhaq <dhiyazhar@gmail.com>
2025-11-19 06:39:09 +0700

    testing webhook

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <p className="header-text">Uji Kompetensi BNSP</p>
         <h1 className="participant-name">{namaPeserta}</h1>
         <p className="scheme-text">System Integrator</p>
-        <p className="scheme-text">v2</p>
+        <p className="scheme-text">v3</p>
 
         
         <div className="deployment-status">
